8933b47dbf16cae8ae20c6e99cd2b5ea69c2c9a9
nazrep <perzankowski.m2001@gmail.com>
2023-06-29 23:10:33 +0200

    WIP

--- src/modules/root/views/WithLayout.vue
+++ src/modules/root/views/WithLayout.vue
@@ -1,28 +1,28 @@
 <template>
   <SidebarTemplate v-if="isSidebarLayout">
     <template #sidebar>
-      <Sidebar />
+      <Sidebar/>
     </template>
 
     <template #content>
-      <router-view />
+      <router-view/>
     </template>
   </SidebarTemplate>
 
-  <router-view v-else />
+  <router-view v-else/>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import {Component, Vue} from 'vue-property-decorator'
 
-import { AllowedTemplates } from '@/shared/contracts/templates'
-import { SidebarTemplate } from '@/shared/templates'
-import { Sidebar } from '@/shared/components/Sidebar'
+import {AllowedTemplates} from '@/shared/contracts/templates'
+import {SidebarTemplate} from '@/shared/templates'
+import {Sidebar} from '@/shared/components/Sidebar'
 
 /**
  * @author Javlon Khalimjonov <javlon.khalimjonov@movecloser.pl>
  */
-@Component({ name: 'WithLayout', components: { SidebarTemplate, Sidebar } })
+@Component({name: 'WithLayout', components: {SidebarTemplate, Sidebar}})
 export class WithLayout extends Vue {
   /**
    * Determines the layout based on the meta parameters passed.
@@ -31,9 +31,10 @@ export class WithLayout extends Vue {
     return this.$route.meta?.template
   }
 
-  public get isSidebarLayout (): boolean {
+  public get isSidebarLayout(): boolean {
     return this.layoutCandidate === AllowedTemplates.Sidebar
   }
 }
+
 export default WithLayout
 </script>
